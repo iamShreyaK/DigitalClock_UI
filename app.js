7bb4438f80f3dc11e0ad0cb89893f90ae8c428ed
Shreya K <shreyakdev@gmail.com>
2024-09-29 09:56:18 +0530

    Add files via upload
    Create a digital clock using HTML/CSS Javascript. Use date class to get hrs,min,sec and set AMPM according to the hour

---
+++ app.js
@@ -0,0 +1,32 @@
+//to start clock at time inteval
+
+setInterval(showTime,1000);
+
+function showTime(){
+    let time = new Date();
+    let hour =time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    //setting time for 12 hours format
+
+    if(hour>=12){
+        if(hour > 12) hour -= 12;
+        am_pm = "PM";
+    }
+    else if(hour ==0){
+        hr =12;
+        am_pm = "AM"
+    }
+
+    hour = hour < 10? "0" + hour : hour;
+    min = min < 10? "0" + min : min;
+    sec = hour < 10? "0" + sec : sec;
+
+    let currentTime = hour + ":" + min + ":" + sec + ":" + am_pm;
+
+    //displaying time 
+    document.getElementById("clock").innerHTML = currentTime;
+}
+showTime()
